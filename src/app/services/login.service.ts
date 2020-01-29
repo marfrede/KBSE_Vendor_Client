f3c1521c9e2c84f0f292a3d9fcc4ad82b4baafb8
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 22:30:37 +0100

    started to write login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Profile } from '../model/profile';
+import { User } from '../model/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  url: string = 'http://localhost:8080/VirtuelleLebensmittel/resources/vendor/';
+  public isLoggedIn:boolean;
+  public profile: Profile;
+  public user:User;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public register(profile: Profile, user: User):Observable<any> {
+    return this.http.post(this.url + "register", JSON.stringify(profile) + "," + JSON.stringify(user));
+  }
+  
+  public login(profile: Profile):Observable<any>{let httpOptions = {
+      responseType: 'text' as 'text',
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post(this.url + "login", JSON.stringify(profile), httpOptions);
+  }
 }
