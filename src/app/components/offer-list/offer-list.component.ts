c68c45f8b535d7e9a8ecf28b6d1ba5ba7028f4ae
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 11:32:55 +0100

    added offer-list component which shows a quantity of offers

---
+++ src/app/components/offer-list/offer-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { OfferService } from '../../services/offer.service';
+
+@Component({
+  selector: 'app-offer-list',
+  templateUrl: './offer-list.component.html',
+  styleUrls: ['./offer-list.component.scss']
+})
+export class OfferListComponent implements OnInit {
+
+  testString:string;
+
+  constructor(
+    private offerService:OfferService
+  ) { }
+
+  ngOnInit() {
+    this.offerService.makeTest().subscribe((test) => {
+      console.log("gettn answer: ", test);
+      this.testString = test;
+    })
+  }
+
+}
