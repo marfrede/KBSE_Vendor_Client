9f5f8dbbb4b42a4b87e31e51f6877ccfb6be7576
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 19:44:22 +0100

    added component to display error messages for the form

---
+++ src/app/components/form-messages/form-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-messages',
+  templateUrl: './form-messages.component.html',
+  styleUrls: ['./form-messages.component.scss']
+})
+export class FormMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
