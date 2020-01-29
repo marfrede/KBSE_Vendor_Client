0199330c0cfe04bc65e809551d961a02fe2da5b4
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 22:27:29 +0100

    created and set up login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../../model/profile';
+import { LoginService } from '../../services/login.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private form:FormGroup;
+  public clickedOnce: boolean;
+
+  private profile:Profile;
+
+  constructor(
+    private loginService:LoginService,
+    private formBuilder:FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.profile = {};
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email, Validators.maxLength(50)]],
+      password: [null, [Validators.required, Validators.maxLength(50)]]
+    });
+  }
+
+  onSubmitLogin() {
+    if(!this.form.valid) {
+      this.clickedOnce = true;
+      return;
+    }
+    this.retrieveValues();
+    console.log(this.profile);
+    this.loginService.login(this.profile).subscribe((answer)=>{
+      console.log("answer:",answer);
+      //FALLUNTERSCHEIDUNG
+      //case success
+      this.setLoggedIn();
+    })
+  }
+
+  retrieveValues() {
+    this.profile.password = this.form.get('password').value;
+    this.profile.username = this.form.get('email').value;
+  }
+
+  setLoggedIn() {
+    this.loginService.isLoggedIn = true;
+    this.loginService.profile = this.profile;
+    this.router.navigate(['']);
   }
 
 }
