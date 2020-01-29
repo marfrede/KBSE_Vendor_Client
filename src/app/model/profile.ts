305d9105c2bdea3888e460df748500d6cb4ce3c0
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 22:29:53 +0100

    added models user and profile(login)

---
+++ src/app/model/profile.ts
@@ -0,0 +1,12 @@
+export class Profile {
+    username?:string;
+    password?:string;
+}
+// @NotNull
+// @Size(min = 2, max = 50)
+// @Column(unique = true)
+// String username;
+
+// @NotNull
+// @Size(min = 2, max = 50)
+// String password;
---
+++ src/app/model/user.ts
@@ -0,0 +1,28 @@
+export class User {
+    id?: number;
+    name?: string;
+    city?: string;
+    street?: string;
+    address?: string;
+    postCode?: number;
+}
+
+// @NotNull
+// @Size(max = 50)
+// protected String name;
+
+// @NotNull
+// @Size(max = 50)
+// protected String street;
+
+// @NotNull
+// @Size(max = 5)
+// protected String address;
+
+// @NotNull
+// @Size(min = 5, max = 5)
+// protected int postCode;
+
+// @NotNull
+// @Size(max = 50)
+// protected String city;
