f3a4c991f78419b423172c40fada7a854d9a2c61
marfrede <marvin.frede@hs-osnabrueck.de>
2020-01-29 11:25:53 +0100

    added app.component, such as app-module and app-routing module where the pathes are defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OfferListComponent } from "./components/offer-list/offer-list.component";
+import { OfferDetailComponent } from "./components/offer-detail/offer-detail.component";
+import { OfferNewComponent } from "./components/offer-new/offer-new.component";
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'all', component: OfferListComponent},
+  {path: 'offer/:id', component: OfferDetailComponent},
+  {path: 'new', component: OfferNewComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfferListComponent } from './components/offer-list/offer-list.component';
+import { OfferDetailComponent } from './components/offer-detail/offer-detail.component';
+import { OfferNewComponent } from './components/offer-new/offer-new.component';
+import { LoginComponent } from './components/login/login.component';
+import { SidebarModule } from "ng-sidebar";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfferListComponent,
+    OfferDetailComponent,
+    OfferNewComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
