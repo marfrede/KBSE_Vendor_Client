277a019eefdede41382b1fe9b51172ec7a313905
marfrede <marvin.frede@hs-osnabrueck.de>
2020-02-03 12:41:54 +0100

    renamed server messages component to message  component

--- src/app/components/messages/messages.component.spec.ts
+++ src/app/components/messages/messages.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ServerMessagesComponent } from './server-messages.component';
+import { MessagesComponent } from './messages.component';
 
-describe('ServerMessagesComponent', () => {
-  let component: ServerMessagesComponent;
-  let fixture: ComponentFixture<ServerMessagesComponent>;
+describe('MessagesComponent', () => {
+  let component: MessagesComponent;
+  let fixture: ComponentFixture<MessagesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServerMessagesComponent ]
+      declarations: [ MessagesComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ServerMessagesComponent);
+    fixture = TestBed.createComponent(MessagesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
