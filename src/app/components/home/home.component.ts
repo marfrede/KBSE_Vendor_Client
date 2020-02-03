c336133f6b16bb236b3c55cb3a7bb659394c40cd
marfrede <marvin.frede@hs-osnabrueck.de>
2020-02-03 16:23:11 +0100

    made link clever (only link if page exists)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
+import { Location } from "@angular/common";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
+
+  frontendActive:boolean;
+  warning:string;
 
   constructor(
-    private loginService: LoginService
+    private location: Location,
+    private loginService: LoginService,
+    private http: HttpClient
   ) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.frontendActive_().then(bool=>this.frontendActive=bool);
+  }
+
+  frontendActive_():Promise<boolean> {
+    return new Promise((resolve) => {
+      this.http.get("http://localhost:8080/VirtuelleLebensmittel/resources/vendor/ping", { observe: 'response' }).subscribe(
+        (s) => {resolve(true);},
+        (e) => {resolve(false);}
+      );
+    });
+  }
+
+  setWarning() {
+    this.frontendActive_().then(bool=>this.frontendActive=bool);
+    this.warning = 'Frontend inaktiv!'
+    setTimeout(()=>{
+      this.warning = null;
+    }, 3000);
   }
 
 }
