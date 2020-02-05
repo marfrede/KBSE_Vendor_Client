d87a6820e6974cc752b79a6951c6514af96c215a
marfrede <marvin.frede@hs-osnabrueck.de>
2020-02-05 08:39:43 +0100

    first finished weight pipe

--- src/app/pipes/weight.pipe.ts
+++ src/app/pipes/weight.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'weight'
+  name: 'weight-format'
 })
 export class WeightPipe implements PipeTransform {
 
   transform(value: number): string {
-    return (value < 1)? value * 100 + ' Cent' : value + ' €';
+    return (value < 1)? value * 1000 + 'g' : value + 'kg';
   }
 
 }
