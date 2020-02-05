0182a2d67f2e83394d21f021ab0ec79145be6550
marfrede <marvin.frede@hs-osnabrueck.de>
2020-02-05 08:38:33 +0100

    first finished price pipe

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'price'
+  name: 'price-format'
 })
 export class PricePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: number): string {
+    return (value < 1)? value * 100 + ' Cent' : value + ' €';
   }
 
 }
