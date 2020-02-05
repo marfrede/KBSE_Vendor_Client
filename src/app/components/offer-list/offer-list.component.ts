66011d3c40315fa634fdacffb7b6cf553245511f
marfrede <marvin.frede@hs-osnabrueck.de>
2020-02-05 08:28:07 +0100

    added icons for (in)active and sort array for active > inactive

--- src/app/components/offer-list/offer-list.component.ts
+++ src/app/components/offer-list/offer-list.component.ts
@@ -32,21 +32,31 @@ export class OfferListComponent implements OnInit {
       this.offerService.getOffers$().subscribe(
         (goodResponse) => {
           this.offers = goodResponse.body as unknown as Offer[];
+          this.offers.sort((o1, o2) => {
+            if (o1.active && !o2.active) {
+              return -1;
+            }
+            if (!o1.active && o2.active) {
+              return 1;
+            }
+            return 0;
+          });
           console.log(this.offers[0]);
         },
         (badResponse) => {
           console.log(badResponse);
           switch (badResponse.status) {
             case 400://bad formatted
-            this.messageService.setMessageTimeout("Server JSON bad formatted Fehler. Versuche es später erneut.", 10000);
+              this.messageService.setMessageTimeout("Server JSON bad formatted Fehler. Versuche es später erneut.", 10000);
               break;
             case 470://token invalid
-            this.router.navigate(['/login']).then(() => this.messageService.setMessage("Ihre Session ist abgelaufen. Bitte melden Sie sich erneut an.", true));
+              console.log("token expired.");
+              this.router.navigate(['/login']).then(() => this.messageService.setMessage("Ihre Session ist abgelaufen. Bitte melden Sie sich erneut an.", true));
               break;
             case 471://token missing
-            this.messageService.setMessage(badResponse.statusMessage);
+              this.messageService.setMessage(badResponse.statusMessage);
               break;
-              default://e.x. 500 server error
+            default://e.x. 500 server error
               this.messageService.setMessage("Server Error >" + badResponse.status + " " + badResponse.statusMessage + "< " + badResponse.body);
               break;
           }
